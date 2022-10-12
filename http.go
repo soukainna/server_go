0dff1ce25a4b008e8db0863e4be4ba0c8880b1cc
soukainna <soukaina1lafdili@gmail.com>
2022-10-12 16:28:10 +0200

    add todo

--- http.go
+++ http.go
@@ -3,11 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello your path is %s ", r)
+		fmt.Fprintf(w, "Hello your path is %s ", r.Host)
+	})
+
+	tmpl := template.Must(template.ParseFiles("todo.html"))
+
+	http.HandleFunc("/to", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO LIST",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
 	})
 
 	fs := http.FileServer(http.Dir("static/"))
