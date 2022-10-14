bb9e522b08658731f1252ab2e2074f6429c74444
soukainna <soukaina1lafdili@gmail.com>
2022-10-14 17:22:06 +0200

    add some collection

--- mongo_go.go
+++ mongo_go.go
@@ -33,5 +33,29 @@ func main() {
 		log.Fatal(err)
 	}
 
+	collection := client.Database("test").Collection("trainers")
+
+	//creer de nouvelles structure Trainer
+	lol := Trainer{"lol", 23, "lol city"}
+	john := Trainer{"john", 21, "john city"}
+	bob := Trainer{"bob", 34, "bob city"}
+
+	//Pour insérer un seul document, utilisez la collection.InsertOne()méthode :
+	insertResult, err := collection.InsertOne(context.TODO(), lol)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//inserer plusieure doc
+	manyInsert := []interface{}{john, bob}
+	insertManyResult, err := collection.InsertMany(context.TODO(), manyInsert)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("connected to mongoDB!")
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+	fmt.Println("Inserted multiple documents: ", insertManyResult.InsertedIDs)
 }
